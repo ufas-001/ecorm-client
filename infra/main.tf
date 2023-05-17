f7ad7ba0e0711c115b0e70e0e1c6f49046e41779
The_Hufaaz <ufasabdulmutolib@gmail.com>
2023-05-17 22:27:07 +0100

    passed the key value with env syntax

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"  # Replace with your desired AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 data "aws_ami" "ubuntu" {
