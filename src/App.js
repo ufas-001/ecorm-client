ea8f97a51c6bfa06dd4afaf2e6dbba39ef4c226e
The_Hufaaz <ufasabdulmutolib@gmail.com>
2023-05-21 10:33:41 +0100

    grant echo to .env persmission

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
     useEffect(() => {
-        const socket = io("ws://localhost:8000");
+        const socket = io(`ws://${REACT_APP_BASE_URL}`);
         socket.off("notification").on("notification", (msgObj, user_id) => {
             // logic for notification
             if (user_id === user._id) {
--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -20,7 +20,7 @@ function CheckoutForm() {
         e.preventDefault();
         if (!stripe || !elements || user.cart.count <= 0) return;
         setPaying(true);
-        const { client_secret } = await fetch("http://localhost:8000/create-payment", {
+        const { client_secret } = await fetch(`http://${process.env.REACT_APP_BASE_URL}/create-payment`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
